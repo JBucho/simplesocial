8f875cf76af1122bfbb64253aa5199aafef57718
Kuba <jakub@buchocki.pl>
2019-04-09 00:15:36 +0200

    do some styling; create home page; add images for tech badges

--- social_site/views.py
+++ social_site/views.py
@@ -1,14 +1,24 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 from posts.models import Post
 
 class HomePage(TemplateView):
     template_name = "index.html"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context["latest_posts"] = Post.objects.all().order_by('-created_at')[:3]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = "Visitor is able to sign up and create User \
+                                  providing nick name (Display name), e-mail \
+                                  address and password. There is no e-mail \
+                                  veryfication (on purpose)."
+        context['group'] = "Visitor is able to see groups list and posts in group. \
+                            After logging in the user can create new group or \
+                            join and leave existing one. Groups can be created \
+                            due to content topic."
+        context['posts'] = "Logged in user can create new post in specific group. \
+                            Post will contain information about creation time and \
+                            its author. The post author is able to delete it."
         return context
-
 
 class WelcomePage(TemplateView):
     template_name = 'welcome.html'
