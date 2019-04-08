0b972cbf44a4f4373b5604282896de7f6373915e
Kuba <jakub@buchocki.pl>
2019-04-09 00:20:25 +0200

    fix displayed text at choice manu at home page

--- social_site/views.py
+++ social_site/views.py
@@ -7,15 +7,15 @@ class HomePage(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user'] = "Visitor is able to sign up and create User \
+        context['message_user'] = "Visitor is able to sign up and create User \
                                   providing nick name (Display name), e-mail \
                                   address and password. There is no e-mail \
                                   veryfication (on purpose)."
-        context['group'] = "Visitor is able to see groups list and posts in group. \
+        context['message_group'] = "Visitor is able to see groups list and posts in group. \
                             After logging in the user can create new group or \
                             join and leave existing one. Groups can be created \
                             due to content topic."
-        context['posts'] = "Logged in user can create new post in specific group. \
+        context['message_posts'] = "Logged in user can create new post in specific group. \
                             Post will contain information about creation time and \
                             its author. The post author is able to delete it."
         return context
