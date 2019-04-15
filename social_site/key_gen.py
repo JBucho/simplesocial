bb8d5c4247c1afb245443233b0a691afa6e5941f
Kuba <jakub@buchocki.pl>
2019-04-15 12:57:05 +0200

    key_gen.py using get_random_secret_key

--- social_site/key_gen.py
+++ social_site/key_gen.py
@@ -1,7 +1,7 @@
 import os
 from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = '3e7#nuv5uj+$)e07m(8%+hy0w_sl0=o4@w+kdacu+m+j$a3th#'
+SECRET_KEY = get_random_secret_key()
 SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def generate_secret_key():
