7684a675e14be00f0aee0c95f69c3d60493a71ab
Buchol <j.buchocki@gmail.com>
2019-04-15 13:07:12 +0200

    small change in file path

--- social_site/key_gen.py
+++ social_site/key_gen.py
@@ -5,5 +5,5 @@ SECRET_KEY = get_random_secret_key()
 SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def generate_secret_key():
-    with open(os.path.join(SETTINGS_DIR, '.secret_key.'), 'w') as kf:
+    with open(os.path.join(SETTINGS_DIR, '.secret_key'), 'w') as kf:
         kf.write('\'' + SECRET_KEY + '\'')
